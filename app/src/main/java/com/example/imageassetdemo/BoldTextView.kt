b70f645235f4a4bcfa0d5e6c686b2eaefca87d3c
Neeraj Gupta <neeraj76@yahoo.com>
2021-08-22 01:12:05 +0530

    Created a custom BoldTextView. We had to copy the fonts in app/src/main/assets folder

---
+++ app/src/main/java/com/example/imageassetdemo/BoldTextView.kt
@@ -0,0 +1,18 @@
+package com.example.imageassetdemo
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class BoldTextView(context: Context, attrs: AttributeSet ): AppCompatTextView(context, attrs) {
+    init {
+        applyFont()
+    }
+
+    private fun applyFont() {
+        val boldTypeFace: Typeface =
+            Typeface.createFromAsset(context.assets, "Montserrat-Bold.ttf")
+        typeface = boldTypeFace
+    }
+}
--- app/src/main/java/com/example/imageassetdemo/SplashActivity.kt
+++ app/src/main/java/com/example/imageassetdemo/SplashActivity.kt
@@ -14,6 +14,6 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
-        }, 2500)
+        }, 500)
     }
 }
