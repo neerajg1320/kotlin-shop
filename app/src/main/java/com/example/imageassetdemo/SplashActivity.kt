652d10a2a18e698e6ccecf7b4261f9942c9f8d79
Neeraj Gupta <neeraj76@yahoo.com>
2021-08-22 00:46:07 +0530

    Transition from SplashActivity to MainActivity

--- app/src/main/java/com/example/imageassetdemo/SplashActivity.kt
+++ app/src/main/java/com/example/imageassetdemo/SplashActivity.kt
@@ -1,11 +1,19 @@
 package com.example.imageassetdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        @Suppress("DEPRECATION")
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 2500)
     }
 }
