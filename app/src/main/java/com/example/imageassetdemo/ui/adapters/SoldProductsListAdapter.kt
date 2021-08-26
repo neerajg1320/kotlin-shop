d2dca5466c4c8ce79042bf57cb80984d31fa245b
Neeraj Gupta <neeraj76@yahoo.com>
2021-08-26 14:00:50 +0530

    Completed the SoldProductDetailsActivity

--- app/src/main/java/com/example/imageassetdemo/ui/adapters/SoldProductsListAdapter.kt
+++ app/src/main/java/com/example/imageassetdemo/ui/adapters/SoldProductsListAdapter.kt
@@ -2,19 +2,20 @@ package com.example.imageassetdemo.ui.adapters
 
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.imageassetdemo.R
-import com.example.imageassetdemo.databinding.ItemListLayoutBinding
-import com.example.imageassetdemo.firestore.FirestoreClass
+
 import com.example.imageassetdemo.models.SoldProduct
-import com.example.imageassetdemo.ui.activities.MyOrderDetailsActivity
-import com.example.imageassetdemo.ui.fragments.BaseFragment
+import com.example.imageassetdemo.ui.activities.SoldProductDetailsActivity
+import com.example.imageassetdemo.util.BoldTextView
 import com.example.imageassetdemo.util.Constants
 import com.example.imageassetdemo.util.GlideLoader
+
 
 /**
  * A adapter class for sold products list items.
@@ -27,7 +28,7 @@ open class SoldProductsListAdapter(
     /**
      * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
      */
-    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    class SoldProductViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
     /**
      * Inflates the item views which is designed in xml layout file
@@ -36,7 +37,7 @@ open class SoldProductsListAdapter(
      * {@link ViewHolder} and initializes some private fields to be used by RecyclerView.
      */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return MyViewHolder(
+        return SoldProductViewHolder(
             LayoutInflater.from(context).inflate(
                 R.layout.item_list_layout,
                 parent,
@@ -58,23 +59,23 @@ open class SoldProductsListAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val model = list[position]
 
-        if (holder is MyViewHolder) {
+        if (holder is SoldProductViewHolder) {
 
-//            GlideLoader(context).loadProductPicture(
-//                model.image,
-//                holder.itemView.iv_item_image
-//            )
-//
-//            holder.itemView.tv_item_name.text = model.title
-//            holder.itemView.tv_item_price.text = "$${model.price}"
-//
-//            holder.itemView.ib_delete_product.visibility = View.GONE
-//
-//            holder.itemView.setOnClickListener {
-//                val intent = Intent(context, SoldProductDetailsActivity::class.java)
-//                intent.putExtra(Constants.EXTRA_SOLD_PRODUCT_DETAILS, model)
-//                context.startActivity(intent)
-//            }
+            GlideLoader(context).loadProductPicture(
+                model.image,
+                holder.itemView.findViewById(R.id.iv_item_image)
+            )
+
+            holder.itemView.findViewById<BoldTextView>(R.id.tv_item_name).text = model.title
+            holder.itemView.findViewById<BoldTextView>(R.id.tv_item_price).text = "$${model.price}"
+
+            holder.itemView.findViewById<ImageButton>(R.id.ib_delete_product).visibility = View.GONE
+
+            holder.itemView.setOnClickListener {
+                val intent = Intent(context, SoldProductDetailsActivity::class.java)
+                intent.putExtra(Constants.EXTRA_SOLD_PRODUCT_DETAILS, model)
+                context.startActivity(intent)
+            }
         }
     }
 
